440f62d99aae2e42d70a0cedb7385a663edfa228
Yisrael Peikes <ypeikes18@gmail.com>
2025-02-14 11:17:06 -0500

    added a couple req grad false flags

--- positional_embeddings.py
+++ positional_embeddings.py
@@ -1,6 +1,8 @@
 import torch as t
 import torch.nn as nn
 
+# Based on  the embeddings desribed in "Attention is all you need"
+# https://arxiv.org/pdf/1706.03762
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model: int) -> None:
         super().__init__()
@@ -8,9 +10,9 @@ class PositionalEncoding(nn.Module):
         
     def forward(self, x: t.Tensor) -> t.Tensor:
         # shape (num_tokens,1)
-        positions = t.arange(0,x.shape[-2]).unsqueeze(-1)
+        positions = t.arange(0,x.shape[-2], requires_grad=False).unsqueeze(-1)
         # shape (1, d_model)
-        denominator = 10000 ** (t.arange(0, self.d_model, 1) / self.d_model).unsqueeze(-2)
+        denominator = 10000 ** (t.arange(0, self.d_model, 1, requires_grad=False) / self.d_model).unsqueeze(-2)
         # shape (num_tokens, d_model)
         func_args = positions / denominator
 
